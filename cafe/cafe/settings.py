69e9dfcc51a7c127a313ef813a3a764ea627cd68
Mykola Halushko <halushkomykola@gmail.com>
2020-03-21 16:57:53 +0200

    django update

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -20,14 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ht^hj3j&!*amsxutnq(a=xhwxi0twnp6ljwjf&v@bp7*v5^(pj'
-
+#SECRET_KEY = 'ht^hj3j&!*amsxutnq(a=xhwxi0twnp6ljwjf&v@bp7*v5^(pj'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ht^hj3j&!*amsxutnq(a=xhwxi0twnp6ljwjf&v@bp7*v5^(pj')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = []
-
-
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 # Application definition
 
 INSTALLED_APPS = [
